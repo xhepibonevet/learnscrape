2d39e97362c611d60925cd41fa32e14b413d6888
Tringa Desku <tringadesku@gmail.com>
2021-12-03 19:39:57 +0100

    nklase

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+import os
+#import xlsxwriter
 
-r = requests.get("https://www.scrapethissite.com/faq/")
+BASE = os.getcwd()
+r = requests.get("https://www.iqair.com/world-air-quality-ranking")
 
 soup = BeautifulSoup(r.text, 'html.parser')
 
+cities = []
+
+table = soup.find("table", attrs={"class": "mb30"})
+for row in table.findAll("tr")[1:]:
+    columns = row.findAll("td")
+    stats = {
+        "city": columns[2].text.split(", ")[0],
+        "state": columns[2].text.split(", ")[1],
+        "aqi": columns[3].text
+    }
+    cities.append(stats)
+
+
+df = pd.DataFrame(cities)
+writer = pd.ExcelWriter(BASE + '\\state.xlsx', engine='xlsxwriter')
+
+df.to_excel(writer, sheet_name='Sheet 1', index=False)
+writer.save()
