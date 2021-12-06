cd5dc47604e989c79b2fe1f270d4f1855e79d823
Rexhep Krasniqi <xhepi@bone.vet>
2021-12-06 18:39:15 +0100

    changed to app to typer cli

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
+import typer
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import os
+import xlsxwriter
+from notify import notification
 
+
+app = typer.Typer()
 BASE = os.getcwd()
 
-r = requests.get("https://www.iqair.com/world-air-quality-ranking")
+def retrieve_cities_list():
+    r = requests.get("https://www.iqair.com/world-air-quality-ranking")
 
-soup = BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(r.text, 'html.parser')
 
-cities = []
+    cities = []
 
-table = soup.find("table", attrs={"class": "mb30"})
-for row in table.findAll("tr")[1:]:
-    columns = row.findAll("td")
-    city, state = columns[2].text.split(",")[0].strip(), columns[2].text.split(",")[1].strip()
-    stats = {
-        "city": city,
-        "state": state,
-        "aqi": columns[3].text
-    }
-    cities.append(stats)
+    table = soup.find("table", attrs={"class": "mb30"})
+    for row in table.findAll("tr")[1:]:
+        columns = row.findAll("td")
+        stats = {
+            "city": columns[2].text.split(",")[0].strip(),
+            "state": columns[2].text.split(", ")[1],
+            "aqi": columns[3].text
+        }
+        cities.append(stats)
+        return cities
 
+def write_xl():
+    df = pd.DataFrame(retrieve_cities_list())
+    writer = pd.ExcelWriter(BASE + '\\state.xlsx', engine='xlsxwriter')
 
-df = pd.DataFrame(cities)
+    df.to_excel(writer, sheet_name='Sheet 1', index=False)
+    writer.save()
 
-writer = pd.ExcelWriter(BASE + '\\air_quality.xlsx', engine='xlsxwriter')
+def notify(qyteti):
+    for city in retrieve_cities_list():
+        if qyteti == city.get('city'):
+            if int(city.get('aqi')) >= 100:
+                notification(qyteti)
 
-df.to_excel(writer, sheet_name='Sheet1', index=False)
-
-writer.save()
-
+if __name__ == "__main__":
+    app()
